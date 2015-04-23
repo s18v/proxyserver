6098724160b6ab98fa6b11d4b2f002e75fc524b4
Saran <sarankumarvellanki@gmail.com>
2015-04-22 20:27:01 -0700

    Initial commit
    Adding index file and package data

---
+++ index.js
@@ -0,0 +1,9 @@
+let http = require('http')
+
+http.createServer((req, res) => {
+    console.log(`Request received at: ${req.url}`)
+    req.pipe(res)
+    for (let header in req.headers) {
+        res.setHeader(header, req.headers[header])    
+    }
+}).listen(8000)
