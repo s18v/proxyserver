b99b8366fb104e8786039f24ac306c807bb2c76c
Saran <sarankumarvellanki@gmail.com>
2015-04-24 01:49:07 -0700

    fix issues
    Unhanded stream error in pipe.

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-let request = require('request')
-let fs = require('fs')
 let http = require('http')
+let fs = require('fs')
+let request = require('request')
+let scheme = 'http://'
 let argv = require('yargs')
     .default('host', '127.0.0.1')
     .argv
-let scheme = 'http://'
 let port = argv.port || argv.host === '127.0.0.1' ? 8000 : 80
 // destinationUrl is built using the --host and --port values
 let destinationUrl = argv.url || scheme  + argv.host + ':' + port
+//let destinationUrl = '127.0.0.1:8000'
 // check if the --log argument is specified and proceed correspondingly
 let outputStream = argv.log ? fs.createWriteStream(argv.log) : process.stdout
 
 // Echo Server
 http.createServer((req, res) => {
-    console.log(`Request received at: ${req.url}`)
+    outputStream.write(`Request received at: ${req.url}`)
     req.pipe(res)
     for (let header in req.headers) {
         res.setHeader(header, req.headers[header])    
@@ -22,25 +23,24 @@ http.createServer((req, res) => {
 
 // Proxy Server
 http.createServer((req, res) => {
-    console.log(`Proxying request to: ${destinationUrl + req.url}`);
-    //Logging the request headers in our server callback
-    outputStream.write('\n\n\n' + JSON.stringify(req.headers))
-    req.pipe(outputStream)
+    outputStream.write(`Proxying request to: ${destinationUrl + req.url}`);
     
     let options = {
         headers: req.headers,
-        url: `http://${destinationUrl}${req.url}`
+        url: `${destinationUrl + req.url}`
     }
-    request(options)
+    
     request(options).pipe(res)
     options.method = req.method
     req.pipe(request(options)).pipe(res)
     
+    //Logging the request headers in our server callback
+    outputStream.write('\n\n\n' + JSON.stringify(req.headers))
+    req.pipe(outputStream)
+    
     //logging the proxy request headers and content
     let downstreamResponse = req.pipe(request(options))
-    outputStream.write(JSON.stringify(downstreamResponse.headers))
     downstreamResponse.pipe(outputStream)
     downstreamResponse.pipe(res)
-    
 }).listen(8001);
 
