fec4f215e6062e7ceb3be4f22ae7355bf315a0ea
Saran <sarankumarvellanki@gmail.com>
2015-04-23 23:16:43 -0700

    add to index
    add yargs. destinationUrl is dynamic. enabled file logging

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 let request = require('request');
 let http = require('http')
-let destinationUrl = '127.0.0.1:8000'
+let argv = require('yargs')
+    .default('host', '127.0.0.1:8000')
+    .argv
+let scheme = 'http://'
+let port = argv.port || argv.host === '127.0.0.1' ? 8000 : 80
+// destinationUrl is built using the --host and --port values
+let destinationUrl = argv.url || scheme  + argv.host + ':' + port
+// check if the --log argument is specified and proceed correspondingly
+let outputStream = argv.log ? fs.createWriteStream(argv.log) : process.stdout
 
 // Echo Server
 http.createServer((req, res) => {
@@ -15,8 +23,9 @@ http.createServer((req, res) => {
 http.createServer((req, res) => {
     console.log(`Proxying request to: ${destinationUrl}`);
     //Logging the request headers in our server callback
-    process.stdout.write('\n\n\n' + JSON.stringify(req.headers))
-    req.pipe(process.stdout)
+    outputStream.write('\n\n\n' + JSON.stringify(req.headers))
+    req.pipe(outputStream)
+    
     let options = {
         headers: req.headers,
         url: `http://${destinationUrl}${req.url}`
@@ -25,10 +34,12 @@ http.createServer((req, res) => {
     request(options).pipe(res)
     options.method = req.method
     req.pipe(request(options)).pipe(res)
+    
     //logging the proxy request headers and content
     let downstreamResponse = req.pipe(request(options))
-    process.stdout.write(JSON.stringify(downstreamResponse.headers))
-    downstreamResponse.pipe(process.stdout)
+    outputStream.write(JSON.stringify(downstreamResponse.headers))
+    downstreamResponse.pipe(outputStream)
     downstreamResponse.pipe(res)
+    
 }).listen(8001);
 
