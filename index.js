9922a2b6e638dd649e729eb8ccc7eabba0b0e30c
Saran Vellanki <sarankumarvellanki@gmail.com>
2015-04-27 21:20:16 -0700

    test commit

--- index.js
+++ index.js
@@ -17,30 +17,29 @@ http.createServer((req, res) => {
     outputStream.write(`Request received at: ${req.url}`)
     req.pipe(res)
     for (let header in req.headers) {
-        res.setHeader(header, req.headers[header])    
+        res.setHeader(header, req.headers[header])
     }
 }).listen(8000);
 
 // Proxy Server
 http.createServer((req, res) => {
     outputStream.write(`Proxying request to: ${destinationUrl + req.url}`);
-    
+
     let options = {
         headers: req.headers,
         url: `${destinationUrl + req.url}`
     }
-    
+
     request(options).pipe(res)
     options.method = req.method
     req.pipe(request(options)).pipe(res)
-    
+
     //Logging the request headers in our server callback
     outputStream.write('\n\n\n' + JSON.stringify(req.headers))
     req.pipe(outputStream)
-    
+
     //logging the proxy request headers and content
     let downstreamResponse = req.pipe(request(options))
     downstreamResponse.pipe(outputStream)
     downstreamResponse.pipe(res)
 }).listen(8001);
-
